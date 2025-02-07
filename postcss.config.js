e104172a5681ee8b5a2de55861434f912780460a
sureshpentapilli <sureshkumarsk72746@gmail.com>
2025-02-07 18:18:18 +0530

    gene3

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    plugins: [
-      require("autoprefixer"),
-      require("cssnano")({ preset: "default" }),
-    ],
+    plugins: {
+      "postcss-preset-env": {},
+      autoprefixer: {},
+     
+    },
   };
   
